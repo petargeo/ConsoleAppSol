d61ff7eb4e28d2328380b8b6be518763e773104f
petargeo <93750179+petargeo@users.noreply.github.com>
2024-11-13 17:08:01 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimpleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call the function to write text to the console
+            WriteToConsole("Hello Petar, this is a message from the console!");
+        }
+
+        // Function to write text to the console
+        static void WriteToConsole(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
